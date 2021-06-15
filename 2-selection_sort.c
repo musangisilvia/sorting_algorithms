bbc4a1714f1b4476b3b96d5bb539323feb63a3a6
musangisilvia <musangisilvia@gmail.com>
2021-06-15 05:08:42 +0000

    print only when there is a swap

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -27,7 +27,7 @@ void selection_sort(int *array, size_t size)
 			temp = array[i];
 			array[i] = array[min];
 			array[min] = temp;
+			print_array((const int *)array, size);
 		}
-		print_array((const int *)array, size);
 	}
 }
