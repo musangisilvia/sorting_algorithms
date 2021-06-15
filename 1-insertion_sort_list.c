a227c1f56dee307b7eec80422eddf8f6c6e7f62e
musangisilvia <musangisilvia@gmail.com>
2021-06-15 07:55:29 +0000

    add check for list with one element

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -9,7 +9,7 @@ void insertion_sort_list(listint_t **list)
 {
 	listint_t *head, *prev;
 
-	if (*list == NULL || list == NULL)
+	if (*list == NULL || list == NULL || (*list)->next == NULL)
 	{
 		return;
 	}
