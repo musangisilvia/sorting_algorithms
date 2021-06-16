2838f2a79c8f438117ccac832c6cb4adcae4412e
musangisilvia <musangisilvia@gmail.com>
2021-06-16 09:27:10 +0000

    add functions for task 3

--- sort.h
+++ sort.h
@@ -24,5 +24,8 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void swap(listint_t *, listint_t *, listint_t **);
 void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+void lomuto_qsort(int *array, int lo, int hi, size_t size);
+int partition(int *array, int lo, int hi, size_t size);
 
 #endif /*SORT_H*/
