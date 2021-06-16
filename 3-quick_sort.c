aa73c2db9e9151c476eaca535c5c4c20f6a8d7bb
musangisilvia <musangisilvia@gmail.com>
2021-06-16 09:25:22 +0000

    add task 3

---
+++ 3-quick_sort.c
@@ -0,0 +1,65 @@
+#include "sort.h"
+
+/**
+  * quick_sort - sorts an array of integers in ascending order.
+  * @array: array to be sorted.
+  * @size: size of the array.
+  *
+  *
+  */
+void quick_sort(int *array, size_t size)
+{
+
+	lomuto_qsort(array, 0, size - 1, size);
+}
+
+/**
+  * lomuto_qsort - sort an array of integers
+  * @array: array to be sorted.
+  * @lo: first element in array.
+  * @hi: last element in array.
+  * @size: size of the array.
+  *
+  */
+void lomuto_qsort(int *array, int lo, int hi, size_t size)
+{
+	int p = 0;
+
+	if (lo < hi)
+	{
+		p = partition(array, lo, hi, size);
+		lomuto_qsort(array, lo, p - 1, size);
+		lomuto_qsort(array, p + 1, hi, size);
+	}
+}
+
+/**
+  * partition - divide the array into into.
+  * @array: array to be partition.
+  * @lo: first element of the array.
+  * @hi: last element in the array.
+  * @size: size of the array.
+  *
+  * Return: the index of the array from where the check should begin.
+  */
+int partition(int *array, int lo, int hi, size_t size)
+{
+	int pivot = array[hi];
+	int i = lo - 1, j, tmp;
+
+	for (j = lo; j <= hi; j++)
+	{
+		if (array[j]  <= pivot)
+		{
+			i++;
+			if (i != j)
+			{
+				tmp = array[i];
+				array[i] = array[j];
+				array[j] = tmp;
+				print_array(array, size);
+			}
+		}
+	}
+	return (i);
+}
